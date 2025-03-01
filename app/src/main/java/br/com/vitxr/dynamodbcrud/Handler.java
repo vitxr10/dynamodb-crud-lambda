1483616def33edbe7d3cdc9ba8b4c07ac75ccb8a
vitor <vitorsalves0812@gmail.com>
2025-03-01 16:55:59 -0300

    first commit

---
+++ app/src/main/java/br/com/vitxr/dynamodbcrud/Handler.java
@@ -0,0 +1,29 @@
+package br.com.vitxr.dynamodbcrud;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+            String output = "{ \"message\": \"hello world\" }";
+
+            return response
+                    .withStatusCode(200)
+                    .withBody(output);
+    }
+
+}
---
+++ app/src/test/java/helloworld/HandlerTest.java
@@ -0,0 +1,23 @@
+package helloworld;
+
+import br.com.vitxr.dynamodbcrud.Handler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class HandlerTest {
+  @Test
+  public void successfulResponse() {
+    Handler handler = new Handler();
+    APIGatewayProxyResponseEvent result = handler.handleRequest(null, null);
+    assertEquals(200, result.getStatusCode().intValue());
+    assertEquals("application/json", result.getHeaders().get("Content-Type"));
+    String content = result.getBody();
+    assertNotNull(content);
+    assertTrue(content.contains("\"message\""));
+    assertTrue(content.contains("\"hello world\""));
+    assertTrue(content.contains("\"location\""));
+  }
+}
